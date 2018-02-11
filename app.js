0566929b28437e33f1f50ff6e553d48c1eab44c6
annaboyatyuk <annaboyatyuk@gmail.com>
2018-02-10 18:27:47 -0800

    the images are different

--- app.js
+++ app.js
@@ -49,70 +49,17 @@ function randomPic() {
 
   var randomIndexThree = Math.floor(Math.random() * allpictures.length);
   thirdImg.src = allpictures[randomIndexThree].filepath;
+  if (randomIndexOne === randomIndexTwo || randomIndexOne === randomIndexThree || randomIndexTwo === randomIndexThree) {
+    randomIndexThree = randomPic();
+  }
   thirdImg.addEventListener('click', randomPic);
   console.log(randomIndexThree);
 }
 randomPic();
-console.log(Pictures.name);
-
-// gen random number for 2nd Image
-// while second img equals firstimg
-// gen new random Image
-
-
-//   for (var i = 0; i < allpictures.length; i++) {
-//     if (secondImg === firstImg)
-//       secondImg = allpictures[i];
-//   }
-//   secondImg.addEventListener('click', randomPic);
-//   secondImg.src = allpictures[randomIndex].filepath;
-
-//   for (var j = 0; j < allpictures.length; j++) {
-//     if (firstImg === secondImg || firstImg === thirdImg || secondImg ===thirdImg)
-//       thirdImg = allpictures[j];
-//   }
-//   thirdImg.src = allpictures[randomIndex].filepath;
-//   thirdImg.addEventListener('click', randomPic);
-// }
+console.log(allpictures.name);
 
 
 
-
-
-
-// var generateRandom = function() {
-//   return Math.floor(Math.random() * allpictures.length);
-// };
-
-
-// var randomPic = function() {
-//   var firstimg =document.getElementById('firstimg');
-//   firstimg = generateRandom();
-//   firstimg.src = allpictures[firstimg].filepath;
-
-//   var secondimg =document.getElementById('secondimg');
-//   secondimg = generateRandom();
-//   secondimg.src = allpictures[secondimg].filepath;
-  
-//   var thirdimg =document.getElementById('thirdimg');
-//   thirdimg = generateRandom();
-//   thirdimg.src = allpictures[thirdimg].filepath;
-// };
-// randomPic();
-
-
-
-// var randomIndex = Math.floor(Math.random() * Pictures.allpictures.length);
-// firstimg.src = Pictures.allpictures[randomIndex].filepath;
-// secondimg.src = Pictures.allpictures[randomIndex].filepath;
-// thirdimg.src = Pictures.allpictures[randomIndex].filepath;
-// }
-
-
-
-// randomPic.firstimg.addEventListener('click', randomPic);
-// randomPic.secondimg.addEventListener('click', radomPic);
-// randomPic.thirdimg.addEventListener('click', radomPic);
 
 
 
